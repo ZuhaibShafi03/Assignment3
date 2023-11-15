42c9605f6bc0b40197a1bebddae32a9b9da19ddb
Zuhaib Shafi <zuhaib.shafi@ontariotechu.net>
2023-11-14 19:29:51 -0500

    Added CRUD Operations

--- server/routes/cal.js
+++ server/routes/cal.js
@@ -5,14 +5,14 @@ let mongoose = require('mongoose');
 //connect with cals model
 
 let Cal = require('../models/cal');
-
+/* CRUD Operation */
 /* Read Operation */
 /* Get Route for the Cal list */
 
 router.get('/', async (req, res, next) => {
     try {
         const callist = await Cal.find().exec();
-        res.render('cal', {
+        res.render('cal/cal', {
             title: 'Cal List',
             Callist: callist
         });
@@ -20,6 +20,87 @@ router.get('/', async (req, res, next) => {
         console.error(err);
         next(err); // Pass the error to the next middleware
     }
+});
+
+/* Add Operation */
+/* Get Route for the displaying add page - Create Operation */
+router.get('/add', async (req, res, next) => {
+    res.render('cal/add',{title:'Add Cal'})
+});
+
+/* Post Route for the processing add page - Create Operation */
+router.post('/add', async (req, res, next) => {
+    try {
+        let newCal = new Cal({
+            "FoodItem": req.body.FoodItem,
+            "Calories": req.body.Calories,
+            "Protein": req.body.Protein,
+            "Carbs": req.body.Carbs,
+            "Fat": req.body.Fat
+        });
+
+        await Cal.create(newCal);  
+        res.redirect('/cal-list');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err.message);
+    }
+});
+
+/* Edit Operation */
+/* Get Route for the displaying Edit Operation - Update Operation */
+router.get('/edit/:id', async (req, res, next) => {
+    let id = req.params.id;
+    Cal.findById(id,(err,calToEdit) =>{
+        if(err)
+        {
+            console.log(err)
+            res.end(err);
+        }
+        else
+        {
+            res.render('cal/edit', {title: 'Edit Cal', cal:calToEdit});
+        }
+    });
+});
+/* Post Route for displaying the Edit Operation - Update Operation */
+router.post('/edit/:id', async (req, res, next) => {
+    let id = req.params.id;
+    let updateCal = Cal({
+        "_id":id,
+        "FoodItem": req.body.FoodItem,
+        "Calories": req.body.Calories,
+        "Protein": req.body.Protein,
+        "Carbs": req.body.Carbs,
+        "Fat": req.body.Fat
+    });
+    Cal.updateOne({_id:id},updateCal,(err) =>{
+        if(err)
+        {
+            console.log(err)
+            res.end(err);
+        }
+        else
+        {
+            res.redirect('/cal-list');
+        }
+    });
+});
+/* Delete Operation */
+/* Get to perform Delete Operation -- deletion */
+router.get('/delete/:id', async (req, res, next) => {
+    let id = req.params.id.trim();
+    Cal.deleteOne({ _id: id })
+    .then(result => {
+        // Handle the result of the delete operation
+        console.log(result);
+        res.redirect('/cal-list');
+    })
+    .catch(err => {
+        // Handle the error
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    });
 });
 
 
